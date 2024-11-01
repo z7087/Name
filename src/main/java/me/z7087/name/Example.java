a8c5915363498f17c877d3d69eb78a4c519c2352
z7087 <75190047+z7087@users.noreply.github.com>
2024-11-01 12:40:48 +0800

    Update Example.java

--- src/main/java/me/z7087/name/Example.java
+++ src/main/java/me/z7087/name/Example.java
@@ -6,65 +6,116 @@ import me.z7087.name.api.MethodAccessor;
 public class Example {
     private static final int i = 0;
     private static final int[] ia = new int[1];
-    private static void a() {
+    private static void a() throws NoSuchFieldException, IllegalAccessException {
         System.out.println("YAY");
+        java.lang.reflect.Field f = Example.class.getDeclaredField("i");
+        f.setAccessible(true);
+        if (f.getInt(null) == -114514)
+            throw new NoSuchFieldException();
     }
+
+    private static void printTime(String name, Runnable runnable) {
+        long t = System.nanoTime();
+        runnable.run();
+        t = System.nanoTime() - t;
+        System.out.println(name + " ok: " + (t / 1000000.0) + "ms");
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private static void printTime2(String name, Runnable runnable) {
+        final String name1 = name + " 1";
+        final String name2 = name + " 2";
+        printTime(name1, runnable);
+        printTime(name2, runnable);
+    }
+
     public static void main(String[] args) throws Throwable {
         System.out.println("main ok");
-        long t = System.nanoTime();
-        {
-            Class<?> doorClass = AccessorClassGenerator.getInstance().getGeneratedDoorClass();
-            assert doorClass != null;
-        }
-        long t1 = System.nanoTime();
-        System.out.println("door class ok: " + (t1-t) / 1000000.0 + "ms");
-        t = System.nanoTime();
-        MethodAccessor<?, ?> method = MethodFactory.create(
-                Example.class.getClassLoader(),
-                Example.class.getDeclaredMethod("a", (Class<?>[]) null),
-                false
-        );
-        method.invoke(null, "unused arg");
-        t1 = System.nanoTime();
-        System.out.println("method accessor class ok: " + (t1-t) / 1000000.0 + "ms");
-        t = System.nanoTime();
-        MethodAccessor<?, ?> method2 = MethodFactory.create(
-                Example.class.getClassLoader(),
-                Example.class.getDeclaredMethod("a", (Class<?>[]) null),
-                false
-        );
-        method2.invoke(null, "unused arg");
-        t1 = System.nanoTime();
-        System.out.println("method accessor class 2 ok: " + (t1-t) / 1000000.0 + "ms");
-        t = System.nanoTime();
-        @SuppressWarnings("unchecked")
-        FieldAccessor<Object, Integer> field = (FieldAccessor<Object, Integer>) FieldFactory.create(
-                Example.class.getClassLoader(),
-                Example.class.getDeclaredField("i")
-        );
-        System.out.println(field.get(null)); // 0
-        field.set(null, 1); // final fields cannot take effect instantly... but why?
-        System.out.println(i); // 0
-        t1 = System.nanoTime();
-        System.out.println("field accessor class ok: " + (t1-t) / 1000000.0 + "ms");
-        t = System.nanoTime();
-        @SuppressWarnings("unchecked")
-        FieldAccessor<Object, Integer> field2 = (FieldAccessor<Object, Integer>) FieldFactory.create(
-                Example.class.getClassLoader(),
-                Example.class.getDeclaredField("i")
-        );
-        System.out.println(field2.get(null)); // 1
-        field2.set(null, 2);
-        System.out.println(i); // 0
-        t1 = System.nanoTime();
-        System.out.println("field accessor class 2 ok: " + (t1-t) / 1000000.0 + "ms");
-        @SuppressWarnings("unchecked")
-        FieldAccessor<Object, int[]> field3 = (FieldAccessor<Object, int[]>) FieldFactory.create(
-                Example.class.getClassLoader(),
-                Example.class.getDeclaredField("ia")
-        );
-        System.out.println(ia.length); // 1
-        field3.set(null, new int[3]); // instantly, but why?
-        System.out.println(ia.length); // 3
+        printTime("door class", new Runnable() {
+            @Override
+            public void run() {
+                Class<?> doorClass = AccessorClassGenerator.getInstance().getGeneratedDoorClass();
+                assert doorClass != null;
+            }
+        });
+        printTime2("method accessor class", new Runnable() {
+            @Override
+            public void run() {
+                MethodAccessor<?, ?> method = null;
+                try {
+                    method = MethodFactory.create(
+                            Example.class.getClassLoader(),
+                            Example.class.getDeclaredMethod("a", (Class<?>[]) null),
+                            false
+                    );
+                } catch (NoSuchMethodException e) {
+                    throw new RuntimeException(e);
+                }
+                method.invoke(null, "unused arg");
+            }
+        });
+        printTime("field accessor class 1", new Runnable() {
+            @SuppressWarnings("unchecked")
+            @Override
+            public void run() {
+                FieldAccessor<Object, Integer> field = null;
+                try {
+                    field = (FieldAccessor<Object, Integer>) FieldFactory.create(
+                            Example.class.getClassLoader(),
+                            Example.class.getDeclaredField("i")
+                    );
+                } catch (NoSuchMethodException e) {
+                    throw new RuntimeException(e);
+                } catch (NoSuchFieldException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(field.get(null)); // 0
+                field.set(null, 1); // final fields cannot take effect instantly... but why?
+                System.out.println(i); // 0
+                System.out.println(field.get(null)); // 1
+            }
+        });
+        printTime("field accessor class 2", new Runnable() {
+            @SuppressWarnings("unchecked")
+            @Override
+            public void run() {
+                FieldAccessor<Object, Integer> field = null;
+                try {
+                    field = (FieldAccessor<Object, Integer>) FieldFactory.create(
+                            Example.class.getClassLoader(),
+                            Example.class.getDeclaredField("i")
+                    );
+                } catch (NoSuchMethodException e) {
+                    throw new RuntimeException(e);
+                } catch (NoSuchFieldException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(field.get(null)); // 1
+                field.set(null, 2);
+                System.out.println(i); // 0
+                System.out.println(field.get(null)); // 2
+            }
+        });
+        printTime("field accessor class 3", new Runnable() {
+            @SuppressWarnings("unchecked")
+            @Override
+            public void run() {
+                FieldAccessor<Object, int[]> field = null;
+                try {
+                    field = (FieldAccessor<Object, int[]>) FieldFactory.create(
+                            Example.class.getClassLoader(),
+                            Example.class.getDeclaredField("ia")
+                    );
+                } catch (NoSuchMethodException e) {
+                    throw new RuntimeException(e);
+                } catch (NoSuchFieldException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(ia.length); // 1
+                field.set(null, new int[3]); // instantly, but why?
+                System.out.println(ia.length); // 3
+                System.out.println(field.get(null).length); // 3
+            }
+        });
     }
 }
